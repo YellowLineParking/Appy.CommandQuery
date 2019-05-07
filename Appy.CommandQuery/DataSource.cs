04159d231d005c635c680c7e5161c3d9f3282f61
Kenneth Truyers <truyers.kenneth@gmail.com>
2019-05-07 17:27:26 +0200

    Update Appy.CommandQuery to use netstandard and use Polly instead of Endjin.Retry as it's retry mechanism

--- Appy.CommandQuery/DataSource.cs
+++ Appy.CommandQuery/DataSource.cs
@@ -1,43 +1,36 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
-using Endjin.Core.Retry;
-using Endjin.Core.Retry.Policies;
-using Endjin.Core.Retry.Strategies;
+using Polly;
 
 namespace Appy.CommandQuery
 {
     public class DataSource : IDataSource
     {
-        readonly IRetryStrategy _retryStrategyStrategy;
-        readonly IRetryPolicy _retryPolicy;
+        readonly Policy _policy;
         readonly Func<Type, object> _getDataSource;
 
         public DataSource(Func<Type, object> getDataSource) =>
             _getDataSource = getDataSource;
 
-        public DataSource(Func<Type, object> getDataSource, IRetryStrategy retryStrategy, IRetryPolicy retryPolicy) : this(getDataSource)
-        {
-            _retryStrategyStrategy = retryStrategy;
-            _retryPolicy = retryPolicy;
-        }
+        public DataSource(Func<Type, object> getDataSource, Policy policy) : this(getDataSource) => 
+            _policy = policy;
 
         T source<T>() =>
             (T) _getDataSource(typeof(T));
 
         public Task<TReturnType> Get<TReturnType, TDataSource>(IQuery<TReturnType, TDataSource> query) => 
-            _retryStrategyStrategy == null 
+            _policy == null 
                 ? query.Get(source<TDataSource>()) 
-            : Retriable.Retry(() => query.Get(source<TDataSource>()), CancellationToken.None, _retryStrategyStrategy, _retryPolicy);
+                : _policy.Execute(() => query.Get(source<TDataSource>()));
 
         public Task<TReturnType> Execute<TReturnType, TDataSource>(ICommand<TReturnType, TDataSource> command) =>
-            _retryStrategyStrategy == null
+            _policy == null
                 ? command.Execute(source<TDataSource>())
-                : Retriable.RetryAsync(() => command.Execute(source<TDataSource>()), CancellationToken.None, _retryStrategyStrategy, _retryPolicy);
+                : _policy.Execute(() => command.Execute(source<TDataSource>()));
 
         public Task Execute<TDataSource>(ICommand<TDataSource> command) =>
-            _retryStrategyStrategy == null
+            _policy == null
                 ? command.Execute(source<TDataSource>())
-                : Retriable.RetryAsync(() => command.Execute(source<TDataSource>()), CancellationToken.None, _retryStrategyStrategy, _retryPolicy);
+                : _policy.Execute(() => command.Execute(source<TDataSource>()));
     }
 }
